88474ce5da649d276a9de1e1ef39d419550c564f
kosuke matsumura <kosuke-matsumura@navitime.co.jp>
2019-04-11 14:14:59 +0900

    call JNI from Kotlin

--- app/src/main/java/com/shcahill/ndksample/MainActivity.kt
+++ app/src/main/java/com/shcahill/ndksample/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.shcahill.ndksample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main).apply {
+            textFromJni.text = stringFromJNI()
+        }
+    }
+
+    private external fun stringFromJNI(): String
+
+    companion object {
+        init {
+            System.loadLibrary("jni")
+        }
     }
 }
